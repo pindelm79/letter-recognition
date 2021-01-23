844c0f9964dff797d4a4ffd6d020475ad553ccae
Michał Pindel <m.pindel7991@gmail.com>
2021-01-23 19:20:42 +0100

    refactor(test): put tests in a class

--- tests/test_nn_layers.py
+++ tests/test_nn_layers.py
@@ -16,60 +16,70 @@ from letter_recognition.nn import layers
         (4, 3, 28, 28, 10, (3, 5), 1, (2, 5), True),
     ],
 )
-def test_conv2d_forward(
-    N, in_channels, in_H, in_W, out_channels, kernel_size, stride, padding, bias
-):
-    conv2d_custom = layers.Conv2d(
+class TestConv2d:
+    def test_conv2d_forward(
+        self,
+        N,
         in_channels,
+        in_H,
+        in_W,
         out_channels,
         kernel_size,
-        stride=stride,
-        padding=padding,
-        bias=bias,
-    )
-    in_shape = (N, in_channels, in_H, in_W)
-    in_array = np.random.random_sample(in_shape)
-    in_tensor = torch.from_numpy(in_array).float()
+        stride,
+        padding,
+        bias,
+    ):
+        conv2d_custom = layers.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        )
+        in_shape = (N, in_channels, in_H, in_W)
+        in_array = np.random.random_sample(in_shape)
+        in_tensor = torch.from_numpy(in_array).float()
 
-    output = conv2d_custom.forward(in_array)
+        output = conv2d_custom.forward(in_array)
 
-    kernel_tensor = torch.from_numpy(conv2d_custom.kernel).float()
-    bias_tensor = torch.from_numpy(conv2d_custom.bias).float()
-    expected = F.conv2d(
-        in_tensor, kernel_tensor, stride=stride, padding=padding, bias=bias_tensor
-    )
+        kernel_tensor = torch.from_numpy(conv2d_custom.kernel).float()
+        bias_tensor = torch.from_numpy(conv2d_custom.bias).float()
+        expected = F.conv2d(
+            in_tensor, kernel_tensor, stride=stride, padding=padding, bias=bias_tensor
+        )
 
-    assert torch.allclose(torch.from_numpy(output).float(), expected)
+        assert torch.allclose(torch.from_numpy(output).float(), expected)
 
-
-@pytest.mark.parametrize(
-    "N, in_channels, in_H, in_W, out_channels, kernel_size, stride, padding, bias",
-    [
-        (1, 1, 50, 100, 1, 3, 1, 0, False),
-        (20, 16, 50, 100, 33, 3, 1, 2, True),
-        (4, 3, 28, 28, 10, (3, 5), 1, (2, 5), True),
-    ],
-)
-def test_conv2d_calculate_output_shape(
-    N, in_channels, in_H, in_W, out_channels, kernel_size, stride, padding, bias
-):
-    conv2d_custom = layers.Conv2d(
+    def test_conv2d_calculate_output_shape(
+        self,
+        N,
         in_channels,
+        in_H,
+        in_W,
         out_channels,
         kernel_size,
-        stride=stride,
-        padding=padding,
-        bias=bias,
-    )
-    in_shape = (N, in_channels, in_H, in_W)
-    in_array = np.random.random_sample(in_shape)
-    in_tensor = torch.from_numpy(in_array).float()
+        stride,
+        padding,
+        bias,
+    ):
+        conv2d_custom = layers.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=bias,
+        )
+        in_shape = (N, in_channels, in_H, in_W)
+        in_array = np.random.random_sample(in_shape)
+        in_tensor = torch.from_numpy(in_array).float()
 
-    out_shape = conv2d_custom.calculate_output_shape(in_shape)
+        out_shape = conv2d_custom.calculate_output_shape(in_shape)
 
-    kernel_tensor = torch.from_numpy(conv2d_custom.kernel).float()
-    expected_shape = F.conv2d(
-        in_tensor, kernel_tensor, stride=stride, padding=padding
-    ).size()
+        kernel_tensor = torch.from_numpy(conv2d_custom.kernel).float()
+        expected_shape = F.conv2d(
+            in_tensor, kernel_tensor, stride=stride, padding=padding
+        ).size()
 
-    assert out_shape == expected_shape
+        assert out_shape == expected_shape
