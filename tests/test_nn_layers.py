a6f8ebe7f1ecd7e1e68270d2491d910c5ccd19e4
Michał Pindel <m.pindel7991@gmail.com>
2021-01-24 01:16:05 +0100

    refactor: test parameters combinations

--- tests/test_nn_layers.py
+++ tests/test_nn_layers.py
@@ -3,41 +3,34 @@ from typing import Tuple
 import numpy as np
 import pytest
 import torch
+import torch.nn
 import torch.nn.functional as F
 
 from letter_recognition.nn import layers
 
 
 @pytest.mark.parametrize(
-    "N, in_channels, in_H, in_W, out_channels, kernel_size, stride, padding, bias",
-    [
-        (1, 1, 50, 100, 1, 3, 1, 0, False),
-        (20, 16, 50, 100, 33, 3, 1, 2, True),
-        (4, 3, 28, 28, 10, (3, 5), 1, (2, 5), True),
-    ],
+    "batch_size, in_channels, out_channels, in_H, in_W, kernel_size",
+    [(1, 1, 1, 4, 4, 3), (50, 1, 9, 28, 28, 3), (69, 9, 17, 29, 13, (2, 5))],
 )
 class TestConv2d:
-    def test_conv2d_forward(
+    @pytest.mark.parametrize("padding", [0, 2, (2, 1)])
+    @pytest.mark.parametrize("bias", [False, True])
+    def test_forward(
         self,
-        N,
+        batch_size,
         in_channels,
         in_H,
         in_W,
         out_channels,
         kernel_size,
-        stride,
         padding,
         bias,
     ):
         conv2d_custom = layers.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias,
+            in_channels, out_channels, kernel_size, padding=padding, bias=bias
         )
-        in_shape = (N, in_channels, in_H, in_W)
+        in_shape = (batch_size, in_channels, in_H, in_W)
         in_array = np.random.random_sample(in_shape)
         in_tensor = torch.from_numpy(in_array).float()
 
@@ -45,41 +38,24 @@ class TestConv2d:
 
         weight_tensor = torch.from_numpy(conv2d_custom.weight).float()
         bias_tensor = torch.from_numpy(conv2d_custom.bias).float()
-        expected = F.conv2d(
-            in_tensor, weight_tensor, stride=stride, padding=padding, bias=bias_tensor
-        )
+        expected = F.conv2d(in_tensor, weight_tensor, bias=bias_tensor, padding=padding)
 
         assert torch.allclose(torch.from_numpy(output).float(), expected)
 
-    def test_conv2d_calculate_output_shape(
-        self,
-        N,
-        in_channels,
-        in_H,
-        in_W,
-        out_channels,
-        kernel_size,
-        stride,
-        padding,
-        bias,
+    @pytest.mark.parametrize("padding", [0, 2, (2, 1)])
+    def test_calculate_output_shape(
+        self, batch_size, in_channels, in_H, in_W, out_channels, kernel_size, padding
     ):
         conv2d_custom = layers.Conv2d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            stride=stride,
-            padding=padding,
-            bias=bias,
+            in_channels, out_channels, kernel_size, padding=padding
         )
-        in_shape = (N, in_channels, in_H, in_W)
+        in_shape = (batch_size, in_channels, in_H, in_W)
         in_array = np.random.random_sample(in_shape)
         in_tensor = torch.from_numpy(in_array).float()
 
         out_shape = conv2d_custom.calculate_output_shape(in_shape)
 
         weight_tensor = torch.from_numpy(conv2d_custom.weight).float()
-        expected_shape = F.conv2d(
-            in_tensor, weight_tensor, stride=stride, padding=padding
-        ).size()
+        expected_shape = F.conv2d(in_tensor, weight_tensor, padding=padding).size()
 
         assert out_shape == expected_shape
