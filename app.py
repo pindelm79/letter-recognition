c8952d2606cbffc2d77a3b5e8e1406f3aecd942c
Michał Pindel <m.pindel7991@gmail.com>
2021-04-23 14:16:50 +0200

    test: readd local imports

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ from flask import Flask, jsonify, request
 import numpy as np
 from PIL import Image, ImageOps
 
-# import letter_recognition.nn.layers as nn
-# import letter_recognition.nn.activation as activation
+import letter_recognition.nn.layers as nn
+import letter_recognition.nn.activation as activation
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Hello, World 2"
+    return "Hello, World 3"
